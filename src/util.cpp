380a1f9e186547ed638ac03bc605f73c6502eb15
BitcoinSolution <bitcoinsolution@gmail.com>
2017-12-21 23:38:46 +0100

    Update util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/bitcoin-config.h"
+#include "config/cryptogaule-config.h"
 #endif
 
 #include "util.h"
@@ -427,7 +427,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(NULL, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "bitcoin";
+    const char* pszModule = "cryptogaule";
 #endif
     if (pex)
         return strprintf(
@@ -448,13 +448,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\CryptoGaule
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\CryptoGaule
+    // Mac: ~/Library/Application Support/CryptoGaule
+    // Unix: ~/.cryptogaule
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "CryptoGaule";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -466,10 +466,10 @@ boost::filesystem::path GetDefaultDataDir()
     // Mac
     pathRet /= "Library/Application Support";
     TryCreateDirectory(pathRet);
-    return pathRet / "Bitcoin";
+    return pathRet / "CryptoGaule";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / "cryptogaule";
 #endif
 #endif
 }
