e289f17392a77dba9ee6f6f552aa5726778138a7
BitcoinSolution <bitcoinsolution@gmail.com>
2017-12-22 13:23:49 +0100

    Update optionsmodel.cpp

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #if defined(HAVE_CONFIG_H)
-#include "config/bitcoin-config.h"
+#include "config/cryptogaule-config.h"
 #endif
 
 #include "optionsmodel.h"
@@ -58,7 +58,7 @@ void OptionsModel::Init()
 
     // Display
     if (!settings.contains("nDisplayUnit"))
-        settings.setValue("nDisplayUnit", BitcoinUnits::BTC);
+        settings.setValue("nDisplayUnit", BitcoinUnits::CRYPTOGAULOIS);
     nDisplayUnit = settings.value("nDisplayUnit").toInt();
 
     if (!settings.contains("strThirdPartyTxUrls"))
