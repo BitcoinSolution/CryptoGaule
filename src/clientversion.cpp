f8930263b412a8750ee87eb25793252b4141cb12
BitcoinSolution <bitcoinsolution@gmail.com>
2017-12-22 11:49:13 +0100

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -10,10 +10,10 @@
 
 /**
  * Name of client reported in the 'version' message. Report the same name
- * for both bitcoind and bitcoin-core, to make it harder for attackers to
+ * for both cryptogauled and cryptogaule-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("CryptoGaule Core");
 
 /**
  * Client version number
