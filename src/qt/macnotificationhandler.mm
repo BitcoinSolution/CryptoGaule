d81c43a5620e58e85b2055f6bb850a3c91eb4d0f
BitcoinSolution <bitcoinsolution@gmail.com>
2017-12-22 13:16:52 +0100

    Update macnotificationhandler.mm

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"org.CryptoGateCorps.CryptoGaule-Qt";
     } else {
         return [self __bundleIdentifier];
     }
