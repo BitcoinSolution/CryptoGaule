142b05d6f35046a6c61d00ee8728889bca132e0b
BitcoinSolution <bitcoinsolution@gmail.com>
2017-12-22 11:50:34 +0100

    Update chainparamsbase.h

--- src/chainparamsbase.h
+++ src/chainparamsbase.h
@@ -9,8 +9,8 @@
 #include <vector>
 
 /**
- * CBaseChainParams defines the base parameters (shared between bitcoin-cli and bitcoind)
- * of a given instance of the Bitcoin system.
+ * CBaseChainParams defines the base parameters (shared between cryptogaule-cli and cryptogauled)
+ * of a given instance of the CryptoGaule system.
  */
 class CBaseChainParams
 {
